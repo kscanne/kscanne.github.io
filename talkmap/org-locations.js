99ae47de4f9ee9ec3f71337c388b886fa53896ae
Kevin Scannell <kscanne@gmail.com>
2023-09-13 18:53:13 +0000

    Update geolocations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,312 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
+    "Dublin, Ireland",
+    53.3493795,
+    -6.2605593
+  ],
+  [
+    "St. Louis, Missouri, USA",
+    38.6280278,
+    -90.1910154
+  ],
+  [
+    "Houston, Texas, USA",
+    29.7589382,
+    -95.3676974
+  ],
+  [
+    "Park City, Utah, USA",
+    40.6460921,
+    -111.4979963
+  ],
+  [
+    "Middletown, Connecticut, USA",
+    41.5623178,
+    -72.6509061
+  ],
+  [
+    "Eugene, Oregon, USA",
+    44.0505054,
+    -123.0950506
+  ],
+  [
+    "Norman, Oklahoma, USA",
+    35.2225717,
+    -97.4394816
+  ],
+  [
+    "Banff, Alberta, Canada",
+    51.175076,
+    -115.5720773
+  ],
+  [
+    "St. John's, Newfoundland, Canada",
+    47.5614705,
+    -52.7126162
+  ],
+  [
+    "Leitir Ceanainn, D\u00fan na nGall, \u00c9ire",
+    54.9517933,
+    -7.7354381
+  ],
+  [
+    "Cambridge, Massachusetts, USA",
+    42.3655767,
+    -71.1040018
+  ],
+  [
+    "Pittsburg, Kansas, USA",
+    37.4108841,
+    -94.70496
+  ],
+  [
+    "Galway, Ireland",
+    53.2744122,
+    -9.0490601
+  ],
+  [
+    "Baile Bh\u00fairne, Contae Chorca\u00ed, \u00c9ire",
+    51.9455757,
+    -9.170658
+  ],
+  [
+    "Aberystwyth, Wales",
+    52.4143113,
+    -4.0816846
+  ],
+  [
+    "Louvain-la-Neuve, Belgium",
+    50.6682012,
+    4.6128839
+  ],
+  [
+    "Limerick, Ireland",
+    52.661252,
+    -8.6301239
+  ],
+  [
+    "Urbana, Illinois, USA",
+    40.1117174,
+    -88.207301
+  ],
+  [
+    "Evanston, Illinois, USA",
+    42.0447388,
+    -87.6930459
+  ],
+  [
+    "Chennai, India",
+    13.0836939,
+    80.270186
+  ],
+  [
+    "Rio Grande, Ohio, USA",
+    38.8795222,
+    -82.380432
+  ],
+  [
+    "Gniezno, Poland",
+    52.52465015,
+    17.598152438162245
+  ],
+  [
+    "Tucson, Arizona, USA",
+    32.2228765,
+    -110.974847
+  ],
+  [
+    "Providence, Rhode Island, USA",
+    41.8239891,
+    -71.4128343
+  ],
+  [
+    "Na Soll\u00e1in, Co. Chill Dara, \u00c9ire",
+    53.249033,
+    -6.665686
+  ],
+  [
+    "Albany, New York, USA",
+    42.6511674,
+    -73.754968
+  ],
+  [
+    "University of Ottawa, Ottawa, Canada",
+    45.42580945,
+    -75.68736498199888
+  ],
+  [
+    "Cambridge, Massachusetts",
+    42.3655767,
+    -71.1040018
+  ],
+  [
+    "South Bend, Indiana",
+    41.6833813,
+    -86.2500066
+  ],
+  [
+    "Cherasco, Italy",
+    44.6522191,
+    7.8580149
+  ],
+  [
+    "Auckland, New Zealand",
+    -36.5412811,
+    174.5506104914412
+  ],
+  [
+    "Shannon Airport, County Clare, Ireland",
+    52.6986845,
+    -8.921554762550244
+  ],
+  [
+    "An Caisle\u00e1n Nua, Contae Longfoirt, \u00c9ire",
+    53.5581824,
+    -7.7092714351586125
+  ],
+  [
+    "Leipzig, Germany",
+    51.3406321,
+    12.3747329
+  ],
+  [
+    "Minde, Portugal",
+    39.5134274,
+    -8.687879
+  ],
+  [
+    "Dublin City University All Hallows Campus, Dublin, Ireland",
+    53.37117739999999,
+    -6.2484877168652195
+  ],
+  [
+    "Douglas, Isle of Man",
+    54.149774,
+    -4.4779021
+  ],
+  [
+    "Pueblo Isleta, New Mexico",
+    35.0246399,
+    -106.6771244
+  ],
+  [
+    "Clayton, Missouri, USA",
+    38.6504352,
+    -90.3362473
+  ],
+  [
+    "Austin, Texas, USA",
+    30.2711286,
+    -97.7436995
+  ],
+  [
+    "College Park, Maryland, USA",
+    38.980666,
+    -76.9369189
+  ],
+  [
+    "Milwaukee, Wisconsin, USA",
+    43.0349931,
+    -87.922497
+  ],
+  [
+    "Kansas City, Missouri, USA",
+    39.100105,
+    -94.5781416
+  ],
+  [
+    "Chester Beatty Museum, Dublin, Ireland",
+    53.3421456,
+    -6.2674294
+  ],
+  [
+    "Indiana University, Bloomington, Indiana, USA",
+    39.1802358,
+    -86.50935262731483
+  ],
+  [
+    "Berkeley, California, USA",
     37.8708393,
-    -122.2728638
+    -122.272863
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "New York Irish Center, Queens, New York, USA",
+    40.7421334,
+    -73.953081
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "University of Illinois, Urbana, Illinois, USA",
+    40.11315635,
+    -88.22479189634599
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Los Angeles, California, USA",
+    34.0536909,
+    -118.242766
+  ],
+  [
+    "Chicago, Illinois, USA",
+    41.8755616,
+    -87.6244212
+  ],
+  [
+    "Bloomington, Indiana, USA",
+    39.1670396,
+    -86.5342881
+  ],
+  [
+    "Athens, Georgia, USA",
+    33.9597677,
+    -83.376398
+  ],
+  [
+    "Na Forbacha, Co. Galway, Ireland",
+    53.2488923,
+    -9.2079295
+  ],
+  [
+    "South Bend, Indiana, USA",
+    41.6833813,
+    -86.2500066
+  ],
+  [
+    "Miami, Florida, USA",
+    25.7741728,
+    -80.19362
+  ],
+  [
+    "Barcelona, Catalonia",
+    41.3828939,
+    2.1774322
+  ],
+  [
+    "Portland State University, Portland, Oregon, USA",
+    45.51181205,
+    -122.68492999382892
+  ],
+  [
+    "University of Cambridge, Cambridge, England",
+    52.210945550000005,
+    0.09200497637871279
+  ],
+  [
+    "Ann Arbor, Michigan, USA",
+    42.2681569,
+    -83.7312291
+  ],
+  [
+    "San Francisco, California, USA",
+    37.7790262,
+    -122.419906
+  ],
+  [
+    "Marseille, France",
+    43.2961743,
+    5.3699525
+  ],
+  [
+    "Batz-sur-Mer, France",
+    47.2771383,
+    -2.4791931
   ]
 ];
